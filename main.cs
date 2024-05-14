3be0dc70a90e228053fc3336ca9b7375faf05694
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-05-14 03:56:33 +0000

    Changed console from displaying one line (hello world) to displaying several lines that provide personal information

--- main.cs
+++ main.cs
@@ -1,7 +1,15 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World! Esteban Amor Is Here");
+  public static void Main(string[] args) {
+    //the following lines display their respective strings in the console
+    Console.WriteLine( "\nLucinda Potter \n6/24/1992\nwork 000-000-0101\ncell 000-000-0189"); 
+
+    Console.WriteLine( "\nJohn Smith \n8/15/1978\nwork 000-111-0101\ncell 000-111-0189");
+
+    Console.WriteLine( "\nChris Johnson \n12/01/1987\nwork 000-222-0101\ncell 000-222-0189");
+
+    Console.WriteLine( "\nRobert Hall \n2/27/1949\nwork 000-444-0101\ncell 000-444-0189");
   }
+
 }
